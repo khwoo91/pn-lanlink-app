b61fb9d1cd76a359c23ef3d369acbdc5bc408889
Your Name <khyunwoo1991@gmail.com>
2026-06-21 12:23:05 +0900

    'update'

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { LitElement, html } from 'lit';
 import { customElement, state, query } from 'lit/decorators.js';
-import { createIcons, Users, Copy, Lock, Square, Info, Crown, ShieldAlert } from 'lucide';
+import { createIcons, Users, Copy, Lock, Square, Info, Crown, ShieldAlert, X } from 'lucide';
 
 // Import components to register them as custom elements
 import './components/common/ll-header';
@@ -33,7 +33,8 @@ export class MyElement extends LitElement {
         Square,
         Info,
         Crown,
-        ShieldAlert
+        ShieldAlert,
+        X
       }
     });
   }
